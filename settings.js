9ed1f0b57579698f80c3c22150cd030fced55ca1
mp5er <michael.pfeiffer.dev@gmail.com>
2018-02-03 13:24:27 +0100

    add settings file

---
+++ settings.js
@@ -0,0 +1,9 @@
+{
+    "workbench.colorTheme": "One Dark Pro",
+    "files.autoSave": "onFocusChange",
+    "html.format.indentInnerHtml": true,
+    "liveServer.settings.donotShowInfoMsg": true,
+    "editor.wordWrap": "on",
+    "git.enableSmartCommit": true,
+    "workbench.iconTheme": "vscode-icons",
+}
