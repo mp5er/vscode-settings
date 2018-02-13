81050adf5db6c052fdbeb7f8ea4625cd69c2aa51
Michael P <mp5er@users.noreply.github.com>
2018-02-13 22:29:12 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,22 @@
 {
-    "workbench.colorTheme": "One Dark Pro",
-    "files.autoSave": "onFocusChange",
-    "html.format.indentInnerHtml": true,
-    "liveServer.settings.donotShowInfoMsg": true,
+    // editor
     "editor.wordWrap": "on",
+    "editor.tabSize": 2,
+    "editor.detectIndentation": false,
+    "editor.insertSpaces": true,
+    // explorer
+    "explorer.confirmDelete": false,
+    "explorer.confirmDragAndDrop": false,
+    // files
+    "files.insertFinalNewline": true,
+    "files.autoSave": "onFocusChange",
+    // git
     "git.enableSmartCommit": true,
+    // html
+    "html.format.indentInnerHtml": true,
+    // live server plugin
+    "liveServer.settings.donotShowInfoMsg": true,
+    // workbench
     "workbench.iconTheme": "vscode-icons",
+    "workbench.colorTheme": "One Dark Pro"
 }
