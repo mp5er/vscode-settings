ee6e14b50796685b25111b19d591b1fd663bf09c
Michael P <mp5er@users.noreply.github.com>
2019-01-20 20:13:03 +0100

    tab -> emmet

--- settings.js
+++ settings.js
@@ -1,34 +1,37 @@
 {
-    // editor
-    "editor.wordWrap": "on",
-    "editor.tabSize": 2,
-    "editor.detectIndentation": false,
-    "editor.insertSpaces": true,
-    "editor.formatOnSave": true,
-    // explorer
-    "explorer.confirmDelete": false,
-    "explorer.confirmDragAndDrop": false,
-    "explorer.openEditors.visible": 0,
-    // files
-    "files.insertFinalNewline": true,
-    "files.autoSave": "onFocusChange",
-    // git
-    "git.enableSmartCommit": true,
-    // html
-    "html.format.indentInnerHtml": true,
-    "html.format.wrapAttributes": "force-aligned",
-    // don't send data to MS
-    "telemetry.enableTelemetry": false,
-    "telemetry.enableCrashReporter": false,
-    // live server
-    "liveServer.settings.donotShowInfoMsg": true,
-    // javascript
-    "javascript.updateImportsOnFileMove.enabled": "always",
-    // workbench
-    "workbench.colorTheme": "One Dark Pro",
-    "workbench.activityBar.visible": false,
-    "workbench.iconTheme": "vscode-icons",
-
-    "window.zoomLevel": 1,
-    "vsicons.dontShowNewVersionMessage": true
-  }
+  // editor
+  "editor.wordWrap": "on",
+  "editor.tabSize": 2,
+  "editor.detectIndentation": false,
+  "editor.insertSpaces": true,
+  "editor.formatOnSave": true,
+  // explorer
+  "explorer.confirmDelete": false,
+  "explorer.confirmDragAndDrop": false,
+  "explorer.openEditors.visible": 0,
+  // files
+  "files.insertFinalNewline": true,
+  "files.autoSave": "onFocusChange",
+  // git
+  "git.enableSmartCommit": true,
+  // html
+  "html.format.indentInnerHtml": true,
+  "html.format.wrapAttributes": "force-aligned",
+  // don't send data to MS
+  "telemetry.enableTelemetry": false,
+  "telemetry.enableCrashReporter": false,
+  // live server
+  "liveServer.settings.donotShowInfoMsg": true,
+  // javascript
+  "javascript.updateImportsOnFileMove.enabled": "always",
+  // workbench
+  "workbench.colorTheme": "One Dark Pro",
+  "workbench.activityBar.visible": false,
+  "workbench.iconTheme": "vscode-icons",
+  // window
+  "window.zoomLevel": 1,
+  // vsicons
+  "vsicons.dontShowNewVersionMessage": true,
+  // emmet
+  "emmet.triggerExpansionOnTab": true
+}
